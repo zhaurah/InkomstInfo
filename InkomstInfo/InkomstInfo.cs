c43e0749014c76ec7efb533f07470cd7befab20f
Zhaurah <zhaurah@gmail.com>
2020-09-10 10:29:51 +0200

    Add project files.

---
+++ InkomstInfo/InkomstInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InkomstInfo
+{
+    class InkomstInfo
+    {
+        public string Namn;
+        public int Inkomst;
+        public int Timmar;
+        public string ShowInkomstInfo()
+        {
+            return Namn + " med inkomst: "+Inkomst;  
+        }
+
+        public string GetNamn()
+        {
+            return Namn;
+        }
+
+    }
+
+}
